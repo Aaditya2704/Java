68f62a5095736c30dd5822651303a571115375a7
Aaditya <aaditya.telange@gmail.com>
2022-10-13 22:56:36 +0530

    updated

--- Pattern/CharacterRightAngle.java
+++ Pattern/CharacterRightAngle.java
@@ -2,7 +2,7 @@ public class CharacterRightAngle {
     public static void main(String[] args) {
         int a = 5;
         char ch = 65;
-        for (int i = 1; i <=a; i++) {
+        for (int i = 1; i <= a; i++) {
             for (int j = 1; j <= i; j++) {
                 System.out.print(ch);
             }
