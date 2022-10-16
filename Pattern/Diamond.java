7b1a7cb0a0cdc46fd7fc0fdf432c549c77d232a3
Aaditya <aaditya.telange@gmail.com>
2022-10-16 13:07:12 +0530

    Diamond pattern done

--- Pattern/Diamond.java
+++ Pattern/Diamond.java
@@ -2,10 +2,19 @@ public class Diamond {
     public static void main(String[] args) {
         int a = 6;
         for (int i = 1; i <= a; i++) {
-            for (int k = a; k > 0; k--) {
+            for (int k = 1; k <= a - i; k++) {
                 System.out.print(" ");
             }
             for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        for (int i = 1; i <= a; i++) {
+            for (int k = 1; k <= i; k++) {
+                System.out.print(" ");
+            }
+            for (int j = a - 1; j >= i; j--) {
                 System.out.print("* ");
             }
             System.out.println();
