8a560869b9b418f67b593cd917e9a85570745924
Aaditya <aaditya.telange@gmail.com>
2022-10-22 23:57:52 +0530

    triangle done

---
+++ Pattern/DownwardTriangle.java
@@ -0,0 +1,14 @@
+public class DownwardTriangle {
+    public static void main(String[] args) {
+        int a = 6;
+        for (int i = 1; i <= a; i++) {
+            for (int k = 1; k <= i; k++) {
+                System.out.print(" ");
+            }
+            for (int j = a - 1; j >= i; j--) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
