dd709c4e726151c05dd8b76707141737c36ffe2f
Aaditya <aaditya.telange@gmail.com>
2022-10-27 22:37:05 +0530

    pattern done

---
+++ Pattern/CombinigTwoTriangle.java
@@ -0,0 +1,21 @@
+public class CombinigTwoTriangle {
+    public static void main(String[] args) {
+        int a = 4;
+
+        for (int i = 1; i <= a; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j);
+            }
+            for (int k = 1; k <= a - i; k++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= a - i; k++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+}
