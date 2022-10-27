1598553ebc8941b592a6dae67aece067f0cd3036
Aaditya <aaditya.telange@gmail.com>
2022-10-27 12:43:47 +0530

    pattern done

---
+++ Pattern/CharacterRightAngle2.java
@@ -0,0 +1,14 @@
+public class CharacterRightAngle2 {
+    public static void main(String[] args) {
+        int a = 5;
+        char ch = 64;
+        for (int i = 1; i <= a; i++) {
+            for (int j = 1; j <= i; j++) {
+                ch = (char) (64 + j);
+                System.out.print(ch);
+            }
+            ch++;
+            System.out.println();
+        }
+    }
+}
