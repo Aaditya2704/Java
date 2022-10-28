1da78d936617eb71945107b6d29be63071e8edea
Aaditya <aaditya.telange@gmail.com>
2022-10-28 19:42:39 +0530

    divisor printed

---
+++ MathProblem/PrintAllDivisor.java
@@ -0,0 +1,11 @@
+public class PrintAllDivisor {
+    public static void main(String[] args) {
+        int a = 36;
+        for (int i = 1; i <= a; i++) {
+            if (a % i == 0) {
+                System.out.print(i + " ");
+            }
+
+        }
+    }
+}
