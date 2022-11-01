d251c01645755ff64651273ce71232708821eba2
Aaditya <aaditya.telange@gmail.com>
2022-11-01 23:34:55 +0530

    updated

--- RecursionProblem/ReverseArray.java
+++ RecursionProblem/ReverseArray.java
@@ -1,4 +1,3 @@
-
 public class ReverseArray {
     static void swap(int i, int j) {
         int temp = i;
@@ -6,20 +5,20 @@ public class ReverseArray {
         j = temp;
     }
 
-    public static void reverse(int a[], int i, int n) {
-        if (i >= n / 2) {
+    static void reverse(int[] a, int i, int N) {
+        if (i >= N / 2) {
             return;
         }
-        swap(a[i], a[n - i - 1]);
-        reverse(a, i + 1, n);
+        swap(a[i], a[N - i - 1]);
+        reverse(a, i + 1, N);
     }
 
     public static void main(String[] args) {
-        int[] arr = { 1, 2, 3, 4, 5 };
-        int length = arr.length;
-        reverse(arr, 0, length - 1);
+        int[] a = { 1, 2, 3, 4, 5, 6 };
+        int length = a.length;
+        reverse(a, 0, length);
         for (int i = 0; i < length; i++) {
-            System.out.print(arr[i] + " ");
+            System.out.print(a[i] + " ");
         }
     }
 }
