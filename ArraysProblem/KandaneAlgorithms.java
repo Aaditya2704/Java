2950a1a8ba903aac2ca2a534c951fd75cf61f230
Aaditya <aaditya.telange@gmail.com>
2022-11-16 16:31:08 +0530

    added new problem

---
+++ ArraysProblem/KandaneAlgorithms.java
@@ -0,0 +1,20 @@
+public class KandaneAlgorithms {
+    public static void main(String[] args) {
+        int[] arr = { -2, 1, -3, 4, -1, 2, 1, -5, 4 };
+        int ans = Solve(arr);
+        System.out.println("The maximum sum of the subarray is " + ans);
+    }
+
+    public static int Solve(int[] arr) {
+        int sum = 0;
+        int max = arr[0];
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+            if (sum > max)
+                max = sum;
+            if (sum < 0)
+                sum = 0;
+        }
+        return max;
+    }
+}
