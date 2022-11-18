b2c92504879c9053e7757d479754f4ce81ffe8ca
Aaditya <aaditya.telange@gmail.com>
2022-11-19 01:04:53 +0530

    added new problem

---
+++ ArraysProblem/LeaderinArray.java
@@ -0,0 +1,18 @@
+public class LeaderinArray {
+    public static void main(String[] args) {
+        int[] arr = { 10, 22, 12, 3, 0, 6 };
+        System.out.println("The leaders of the second array are: ");
+        printLeaders(arr, arr.length);
+    }
+
+    public static void printLeaders(int[] a, int length) {
+        int max = a[length - 1];
+        System.out.print(a[length - 1] + " ");
+        for (int i = length - 1; i >= 0; i--) {
+            if (a[i] > max) {
+                System.out.print(a[i] + " ");
+                max = a[i];
+            }
+        }
+    }
+}
