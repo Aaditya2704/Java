62a78af9d0c6ddc3b5d68b5e9bf8ea500a65bab3
Aaditya <aaditya.telange@gmail.com>
2022-11-24 23:02:36 +0530

    updated

--- ArraysProblem/StockBuyAndSell.java
+++ ArraysProblem/StockBuyAndSell.java
@@ -1,6 +1,6 @@
 public class StockBuyAndSell {
     public static void main(String[] args) {
-        int[] arr = { 7, 1, 5, 3, 6, 4 };
+        int[] arr = { 1, 4, 2, 9, 6 };
         int maxPro = maxProfit(arr);
         System.out.println("Max profit is " + maxPro);
     }
