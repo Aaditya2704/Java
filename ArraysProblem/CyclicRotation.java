89edbe382d4237a301cf63b4d960b7332bf4d13c
Aaditya <aaditya.telange@gmail.com>
2023-01-23 19:18:11 +0530

    updated

--- ArraysProblem/CyclicRotation.java
+++ ArraysProblem/CyclicRotation.java
@@ -17,8 +17,10 @@ public class CyclicRotation {
 
     public static void main(String[] args) {
         try (Scanner sc = new Scanner(System.in)) {
+            System.out.print("Enter the size of the array:");
             int size = sc.nextInt();
             int[] a = new int[size];
+            System.out.print("Enter the array:");
             for (int i = 0; i < size; i++) {
                 a[i] = sc.nextInt();
             }
